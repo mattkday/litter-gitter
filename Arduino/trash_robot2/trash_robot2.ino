42e10f9227d191f1c2e0a248afb6482c05d80a14
mattkday00 <mattkday00@gmail.com>
2017-01-20 17:32:30 -0600

    created arduino directory to hold the arduino file

---
+++ Arduino/trash_robot2/trash_robot2.ino
@@ -0,0 +1,68 @@
+int x;
+int y;
+int w = 480;
+int h = 320;
+int leftTrig = w*0.4;
+int rightTrig = w*0.6;
+int leftLed = 10;
+int rightLed = 11;
+
+void setup(){
+  Serial.begin(9600);
+  pinMode(leftLed, OUTPUT);
+  pinMode(rightLed, OUTPUT);
+}
+
+void loop(){
+    //stop motors
+    //drive and avoid obstacles
+}
+
+void serialEvent(){
+  int tx, ty;
+   while (Serial.available()>0) {
+     int tx = Serial.parseInt();
+     int ty = Serial.parseInt();
+     if (tx >0 && ty >0 && Serial.available() < 2) {
+         x = tx;
+         y = ty;
+         getObject();
+         Serial.println(x);
+         Serial.println(y);
+       }  
+  }
+  
+}
+
+void getObject(){
+  //stop motors
+  digitalWrite(leftLed, LOW);
+  digitalWrite(rightLed, LOW);
+  if (x < leftTrig || x == leftTrig) {
+   //turn left
+   Serial.println("left");
+   digitalWrite(leftLed, HIGH);
+   delay(250);
+   digitalWrite(leftLed, LOW);
+   delay(250);
+  } else if (x > rightTrig || x == rightTrig) {
+   //turn right
+   Serial.println("right");
+   digitalWrite(rightLed, HIGH);
+   delay(250);
+   digitalWrite(rightLed, LOW);
+   delay(250);
+  } else if (x > leftTrig && x < rightTrig) {
+    //drive forward
+    Serial.println("middle");
+    //see if close to capture
+   digitalWrite(leftLed, HIGH);
+   digitalWrite(rightLed, HIGH);
+   delay(250);
+   digitalWrite(leftLed, LOW);
+   digitalWrite(rightLed, LOW);
+   delay(250);
+  } else {
+    Serial.println("No Value");
+  }
+}
